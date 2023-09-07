fad2c0560bde895714627d10131505128c3f431b
Joel Alexander <a.joeljr@hotmail.com>
2023-09-07 14:08:45 -0700

    feat: initial version

--- src/api-client.ts
+++ src/api-client.ts
@@ -11,7 +11,7 @@ interface RequestConfig {
 
 export class HTTPClient {
   private static instance: HTTPClient;
-  private baseURL: string = 'https://optimus-prompt-backend.vercel.app/api/parea/v1'; // 'http://127.0.0.1:8000/api/parea/v1';
+  private baseURL: string = 'https://optimus-prompt-backend.vercel.app/api/parea/v1';
   private apiKey: string | null = null;
   private client: AxiosInstance;
 
