31adc24f21e93e9fc5dff43ae519aa8bd67213cc
Joel Alexander <a.joeljr@hotmail.com>
2024-01-29 09:58:35 -0500

    feat(tracer): experiment, evals, new context manager

--- src/api-client.ts
+++ src/api-client.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosInstance, AxiosResponse, AxiosError } from 'axios';
+import axios, { AxiosError, AxiosInstance, AxiosResponse } from 'axios';
 import axiosRetry from 'axios-retry';
 
 interface RequestConfig {
@@ -11,7 +11,7 @@ interface RequestConfig {
 
 export class HTTPClient {
   private static instance: HTTPClient;
-  private baseURL: string = 'http://127.0.0.1:8000/api/parea/v1'; // https://parea-ai-backend-e2adf7624bcb3980.onporter.run/api/parea/v1';
+  private baseURL: string = 'https://parea-ai-backend-e2adf7624bcb3980.onporter.run/api/parea/v1';
   private apiKey: string | null = null;
   private client: AxiosInstance;
 
@@ -26,21 +26,6 @@ export class HTTPClient {
 
     this.client.interceptors.request.use(this.requestInterceptor);
     this.client.interceptors.response.use(this.responseInterceptor, this.errorInterceptor);
-  }
-
-  private requestInterceptor(config: any) {
-    // TBD: Add any request modifications here
-    return config;
-  }
-
-  private responseInterceptor(response: AxiosResponse) {
-    // TBD: Add any response modifications here
-    return response;
-  }
-
-  private errorInterceptor(error: AxiosError) {
-    // TBD: Add any error modifications here
-    return Promise.reject(error);
   }
 
   public static getInstance(): HTTPClient {
@@ -68,5 +53,17 @@ export class HTTPClient {
       console.error(`Request to ${config.endpoint} failed with error ${error}`);
       throw error;
     }
+  }
+
+  private requestInterceptor(config: any) {
+    return config;
+  }
+
+  private responseInterceptor(response: AxiosResponse) {
+    return response;
+  }
+
+  private errorInterceptor(error: AxiosError) {
+    return Promise.reject(error);
   }
 }
