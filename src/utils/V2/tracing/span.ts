b8081d779edf2f5f4b6c5dc5551accdc98f3aab7
Joel Alexander <a.joeljr@hotmail.com>
2024-07-13 18:18:58 -0400

    fix(experiments): revert to 1.47 add back some commits

--- src/utils/V2/tracing/span.ts
+++
@@ -1,6 +0,0 @@
-// import { Span, SpanContext, trace } from '@opentelemetry/api';
-//
-// export function createSpan(name: string, context?: SpanContext): Span {
-//   const tracer = trace.getTracer('openai-wrapper');
-//   return tracer.startSpan(name, undefined, context);
-// }
