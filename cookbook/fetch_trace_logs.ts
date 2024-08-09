4c44f316276f56722dd5902c8770eb313e7d7ed7
Joschka Braun <joschka.braun@gmail.com>
2024-08-09 15:47:00 -0400

    docs: cleanup cookbook example

--- cookbook/fetch_trace_logs.ts
+++ cookbook/fetch_trace_logs.ts
@@ -6,8 +6,6 @@ dotenv.config();
 const p = new Parea(process.env.PAREA_API_KEY);
 
 async function main() {
-  const experiments = await p.listExperiments();
-  console.log(`found ${experiments.length} experiments`);
   const response: PaginatedTraceLogsResponse = await p.getTraceLogs({
     project_name: 'default',
     filter_field: 'trace_name',
